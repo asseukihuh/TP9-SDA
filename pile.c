65d39413d0aa49008a82733433d2799ade20accb
Ny-Harena RAKOTOVAO <107315159+asseukihuh@users.noreply.github.com>
2025-04-01 16:50:43 +0200

    dadsadsa
    dsadsadasd

--- pile.c
+++ pile.c
@@ -2,54 +2,71 @@
 
 
 
+//Fonctionne
+
 void initPile( T_Pile * P)
 {
+    P->nbElts = 0;
 }
 
+//Fonctionne
 
 int pilepleine(const  T_Pile *P)
 {
-return 0;
+    return P->nbElts == MAX;
 }
 
-
+//Fonctionne
 
 int pilevide(const  T_Pile *P)
 {
-return 1;
+    return P->nbElts == 0;
 }
 
+//Fonctionne
 
+int empiler(T_Pile * P, T_Elt e) {
+    if (pilepleine(P)) 
+        return 0; 
+    affecterElt(&P->Elts[P->nbElts], &e); 
+    P->nbElts++;
+    return 1; 
+}
 
-int empiler( T_Pile *P, T_Elt e) //renvoie 0 si pile pleine, sinon 1
+//Fonctionne
+
+int depiler( T_Pile * P, T_Elt *pelt)  //renvoie 0 si pile vide, sinon 1
 {
-return 0;
+    if (pilevide(P))
+        return 0; 
+    affecterElt(pelt, &P->Elts[P->nbElts -1]);
+    P->nbElts--;
+    return 1; 
 }
 
+//Fonctionne
 
-
-int depiler( T_Pile *P, T_Elt *pelt)  //renvoie 0 si pile vide, sinon 1
-{
-return 0;
+T_Elt sommet(const  T_Pile *P) {
+    if (pilevide(P)) return 0;
+    afficherElt(&P->Elts[P->nbElts - 1]);
+    return 1; 
 }
 
-
-
-T_Elt sommet(const  T_Pile *P)
-{
-return 0;
-}
-
-
+//Fonctionne
 
 int hauteur(const  T_Pile *P)
 {
-return 0;
+    return P->nbElts;
 }
 
-
-void afficherPile(  T_Pile *P)
+void afficherPile(T_Pile * P) 
 {
+    T_Pile temp = *P;
+    T_Elt elt;
+    while (!pilevide(&temp)) { 
+        depiler(&temp, &elt); 
+        afficherElt(&elt);
+    } 
 }
 
 
--- tp9.c
+++ tp9.c
@@ -3,6 +3,8 @@
 #include "pile.h"
 #include "file.h"
 
+int testPile();
+
 
 int menu()
 {
@@ -19,7 +21,26 @@ scanf("%d",&choix);
 return choix;
 }
 
-int main()
+int menu2()
+{
+
+int choix2;
+printf("\n\n\n TEST PILE");
+printf("\n\n\n 1 : initialiser");
+printf("\n 2 : pilevide");
+printf("\n 3 : pilepleine");
+printf("\n 4 : empiler");
+printf("\n 5 : depiler");
+printf("\n 6 : sommet");
+printf("\n 7 : hauteur");
+printf("\n 8 : afficher");
+printf("\n 0 :  QUITTER  ");
+printf("\n votre choix ?  ");
+scanf("%d",&choix2);
+return choix2;
+}
+
+int main(T_Pile * P)
 {
 T_File mafile;
 T_Pile mapile;
@@ -27,16 +48,24 @@ int chx;
 // int taille;
 //char chaine[20];
 
+T_Elt element;
+T_Elt peltt;
+
 do
 {
 chx=menu();
 switch (chx)
 	{
 	case 1 :  
-		//testez toutes vos fonctions par un jeu de test de votre choix
-		//testPile(&mapile); //TP9 partie 1 : à ecrire 
+		testPile(&mapile);
 		break;
 	case 2 : //testez toutes vos fonctions par un jeu de test de votre choix
+		//initPile(&mapile);
+		//empiler(&mapile, elementtest);
+		//empiler(&mapile, elementtest1);
+		//empiler(&mapile, elementtest);
+		//empiler(&mapile, elementtest1);
+		//sommet(&mapile);
 		// testFile(&mafile); //TP9 partie 1 : à ecrire 
 		break; 
 	case 3 : 
@@ -55,4 +84,57 @@ switch (chx)
 
 printf("\nau plaisir de vous revoir ...\n");
 return 0;
+}
+
+int testPile(T_Pile * P)
+{
+	int chx2;
+
+	do
+	{
+	chx2=menu2();
+	switch (chx2)
+		{
+		case 1 :  
+			initPile(&mapile);
+			break;
+		case 2 : 
+			printf("\n");
+			if(pilevide(&mapile))
+				printf("la pile est vide !")
+			else 
+				printf("la pile n'est pas vide !")
+			break; 
+		case 3 :
+			printf("\n");
+			if(pilepleine(&mapile))
+				printf("la pile est pleine !")
+			else 
+				printf("la pile n'est pas pleine !")
+			break; 
+		case 4 : 
+			printf("\n");
+			printf("saisir un element : ");
+			saisirElt(&element);
+			empiler(&mapile, element);
+			break;
+		case 5 : 
+			depiler(&mapile, peltt);
+			break;
+		case 6 : 
+			printf("Le sommet de la pile est : ");
+			sommet(&mapile);
+			break;
+		case 7 : 
+			printf("La hauteur de la pile est : %d", hauteur(&mapile));
+			break;
+		case 8 : 
+			printf("Affichage de la pile : ");
+			afficherPile(&mapile);
+			break;
+	}while(chx!=0);
+	
+	printf("\nau plaisir de vous revoir ...\n");
+	return 0;
+	}
 }
